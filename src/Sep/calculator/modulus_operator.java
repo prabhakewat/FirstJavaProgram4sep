1d302eb7639a6c7c2d536f5fd0e3e2b8573d52e8
Prabhakewat <prabhakewat7@gmail.com>
2024-09-15 22:35:17 +0530

    This is my task i have done

---
+++ src/Sep/calculator/modulus_operator.java
@@ -0,0 +1,16 @@
+package Sep.calculator;
+
+public class modulus_operator {
+
+    public static void main(String[]args){
+
+        double a = 34;
+        double b = 10;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
