03e7329053ece9958b4acdfdd30bedfa516c71cd
Prabhakewat <prabhakewat7@gmail.com>
2024-09-17 23:04:14 +0530

    This is my task i have done

--- src/All_Task/Diffrence_double_equal.java
+++ src/All_Task/Diffrence_double_equal.java
@@ -5,7 +5,7 @@ public class Diffrence_double_equal {
     public static void main(String[]args) {
 
         int x = 30;
-        int y = 30;00
+        int y = 30;
         System.out.println("value of x:"+ x);
         System.out.println("value of y:"+ y);
 
